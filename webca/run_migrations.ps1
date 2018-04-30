614f758300b85e5c833214aca144f9c4ef7308a8
Jesús Fernández <jesus@nublar.net>
2018-04-30 16:43:16 +0200

    Updated migration script to find correctly all the available migrations

--- webca/run_migrations.ps1
+++ webca/run_migrations.ps1
@@ -1,11 +1,15 @@
 python manage.py makemigrations
 $v = Read-Host -Prompt "Apply migrations?"
-if ($v)
-{
+if ($v) {
     python manage.py migrate admin
     python manage.py migrate auth
     python manage.py migrate contenttypes
     python manage.py migrate sessions
     python manage.py migrate web
-    python manage.py migrate certstore_db --database certstore_db
+}
+
+python manage.py makemigrations --settings webca.ca_admin.settings
+$v = Read-Host -Prompt "Apply migrations?"
+if ($v) {
+    python manage.py migrate certstore_db --database certstore_db --settings webca.ca_admin.settings
 }
