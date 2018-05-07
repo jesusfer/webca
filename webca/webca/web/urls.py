8b33e236086e3538acc539188581f06c233e9203
Jesus Fernandez <jesus@nublar.net>
2018-05-07 23:11:10 +0200

    Added CSR how-to

--- webca/webca/web/urls.py
+++ webca/webca/web/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from webca.web.views import requests
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('new/', requests.NewView.as_view(), name='new'),
     path('submit/', requests.SubmitView.as_view(), name='submit'),
     path('ok/', requests.request_confirmation, name='ok'),
+    path('examples/', TemplateView.as_view(template_name="webca/web/requests/examples.html"), name='examples'),
 ]
