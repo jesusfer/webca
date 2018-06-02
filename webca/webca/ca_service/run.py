87593fe0b2a7144c829571fa5728bba73adaf0a8
Jesus Fernandez <jesus@nublar.net>
2018-06-03 01:04:18 +0200

    Update pythonpath in run.py to make it more standalone

--- webca/webca/ca_service/run.py
+++ webca/webca/ca_service/run.py
@@ -1,8 +1,20 @@
 """Import this module to run the service"""
+import os
+import sys
 
 import django
+
+BASE_DIR = os.path.abspath(os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    os.path.pardir,
+    os.path.pardir,
+))
+sys.path.append(BASE_DIR)
+os.environ["DJANGO_SETTINGS_MODULE"] = "webca.ca_service.settings"
+
 django.setup()
 
 #pylint: disable=c0413
 from webca.ca_service.service import CAService
+
 CAService().run()
